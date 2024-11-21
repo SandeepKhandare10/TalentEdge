e842b47246ff6ecc21bd30619dbb1bdf54bc2140
SaumyaPatil <saupatil10dec@gmail.com>
2024-11-21 16:58:20 +0530

    Designing user-information-show-step2 page

---
+++ src/components/Component1/Component1.js
@@ -0,0 +1,14 @@
+const Component1 = () =>{
+    return(
+        <div className="master-container">
+            <div className="left-container">
+                <div className="left-inner-container"></div>
+            </div>
+            <div className="right-container">
+                <div className="right-inner-container"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Component1;
---
+++ src/pages/EducationalDetails/EducationalDetails.js
@@ -0,0 +1,11 @@
+import EducationalDetailsCard from "../../components/EducationalDetails/EducationalDetailsCard";
+import './EducationalDetails.css';
+const EducationalDetails = () =>{
+    return(
+        <div className="EduDetailsCont">
+            <EducationalDetailsCard/>
+        </div>
+    );
+}
+
+export default EducationalDetails;
